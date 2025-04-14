1de49729f21c25ee7b6712a007d38bb646404d22
Tomasz Bury <32302797+TomaszBury@users.noreply.github.com>
2025-04-14 16:30:24 +0200

    Section 2 - done

---
+++ section_02/CPPProject/project_001/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Hello Tom command-line" << std::endl;
+    return 0;
+}
---
+++ section_02/CPPProject/project_001/readme.md
@@ -0,0 +1,6 @@
+```console
+    g++ -Wall -std=c++14 main.cpp
+```
+```bash
+g++ -Wall -std=c++14 main.cpp -o main
+```
